36b3eeb25cc5bef6f22d2990e9cfaeb021f63daf
JohnR46 <Johnnote7@gmail.com>
2022-03-16 14:00:37 +0700

    remove debug for db

--- app/dao/database.py
+++ app/dao/database.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import sessionmaker
 from settings import DB_USER, DB_PASS, DB_URL
 
 SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}".format(DB_USER, DB_PASS, DB_URL)
-print("SQLALCHEMY_DATABASE_URL : {}".format(SQLALCHEMY_DATABASE_URL))
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
